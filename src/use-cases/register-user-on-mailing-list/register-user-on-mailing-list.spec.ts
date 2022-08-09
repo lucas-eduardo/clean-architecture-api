e19d9b55ee0ba7afc395e33e2248e07a95028ed6
Lucas Eduardo <contato@lucaseduardo.io>
2022-08-09 20:16:06 -0300

    feat: user data interface

---
+++ src/use-cases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -0,0 +1,20 @@
+import { UserData } from './user-data'
+
+describe('Register user on mailing list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users)
+    // const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+
+    // const name = 'john doe'
+    // const email = 'john@doe.com'
+
+    // const response = await useCase.registerUserOnMailingList({ name, email })
+
+    // const user = repo.findUserByEmail(email)
+
+    // expect((await user).name).toBe(name)
+  })
+})
