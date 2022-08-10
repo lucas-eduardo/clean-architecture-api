005046f94cdd354cb57760368d28d80d4d759c3e
Lucas Eduardo <contato@lucaseduardo.io>
2022-08-10 15:16:15 -0300

    feat: express route adapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express'
+
+import { RegisterUserController } from '@/web-controllers'
+import { HttpRequest } from '@/web-controllers/ports'
+
+export const adaptRoute = (controller: RegisterUserController) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: HttpRequest = {
+      body: req.body
+    }
+
+    const httpResponse = await controller.handle(httpRequest)
+
+    res.status(httpResponse.statusCode).json(httpResponse.body)
+  }
+}
