985e1297677aa26ddb128e63c0dba2f97a43051b
Lucas Eduardo <contato@lucaseduardo.io>
2022-08-10 10:59:44 -0300

    feat: web controller should return server raises

--- src/web-controllers/register-user.controller.ts
+++ src/web-controllers/register-user.controller.ts
@@ -1,35 +1,39 @@
 import { UserData } from '@/entities'
-import { RegisterUserOnMailingList } from '@/use-cases/register-user-on-mailing-list'
+import { UseCase } from '@/use-cases/ports'
 
 import { MissingParamError } from './errors'
 import { HttpRequest, HttpResponse } from './ports'
-import { badRequest, created } from './util'
+import { badRequest, created, serverError } from './util'
 
 export class RegisterUserController {
-  private readonly useCase: RegisterUserOnMailingList
+  private readonly useCase: UseCase
 
-  constructor (useCase: RegisterUserOnMailingList) {
+  constructor (useCase: UseCase) {
     this.useCase = useCase
   }
 
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    if (!request.body.name || !request.body.email) {
-      let missingParam = !(request.body.name) ? 'name ' : ''
-      missingParam += !(request.body.email) ? 'email' : ''
+    try {
+      if (!request.body.name || !request.body.email) {
+        let missingParam = !(request.body.name) ? 'name ' : ''
+        missingParam += !(request.body.email) ? 'email' : ''
 
-      return badRequest(new MissingParamError(missingParam.trim()))
-    }
+        return badRequest(new MissingParamError(missingParam.trim()))
+      }
 
-    const userData: UserData = request.body
+      const userData: UserData = request.body
 
-    const response = await this.useCase.registerUserOnMailingList(userData)
+      const response = await this.useCase.perform(userData)
 
-    if (response.isLeft()) {
-      return badRequest(response.value)
-    }
+      if (response.isLeft()) {
+        return badRequest(response.value)
+      }
 
-    if (response.isRight()) {
-      return created(response.value)
+      if (response.isRight()) {
+        return created(response.value)
+      }
+    } catch (error) {
+      return serverError(error)
     }
   }
 }
