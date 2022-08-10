24b2a8322bf8fed6e2be2858e3c8a4686b36499f
Lucas Eduardo <contato@lucaseduardo.io>
2022-08-10 09:10:59 -0300

    test: register valid user to mailing list

--- src/use-cases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/use-cases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,20 +1,24 @@
 import { UserData } from '@/entities/user-data'
 
+import { UserRepository } from './ports/user-repository'
+import { RegisterUserOnMailingList } from './register-user-on-mailing-list'
+import { InMemoryUserRepository } from './repository/in-memory-user-repository'
+
 describe('Register user on mailing list use case', () => {
   test('should add user with complete data to mailing list', async () => {
     const users: UserData[] = []
 
-    console.log(users)
-    // const repo: UserRepository = new InMemoryUserRepository(users)
-    // const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
 
-    // const name = 'john doe'
-    // const email = 'john@doe.com'
+    const name = 'john doe'
+    const email = 'john@doe.com'
 
-    // const response = await useCase.registerUserOnMailingList({ name, email })
+    const response = await useCase.registerUserOnMailingList({ name, email })
 
-    // const user = repo.findUserByEmail(email)
+    const user = repo.findUserByEmail(email)
 
-    // expect((await user).name).toBe(name)
+    expect((await user).name).toBe(name)
+    expect(response.value.name).toBe(name)
   })
 })
