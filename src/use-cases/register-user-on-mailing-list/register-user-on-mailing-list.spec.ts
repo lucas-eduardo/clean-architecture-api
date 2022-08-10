eb70090b2db2348c039f8b955bf0319c62f591e0
Lucas Eduardo <contato@lucaseduardo.io>
2022-08-10 09:19:04 -0300

    test: prevent to add user with invalid name

--- src/use-cases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ src/use-cases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,4 +1,5 @@
 import { InvalidEmailError } from '@/entities/errors/invalid-email-error'
+import { InvalidNameError } from '@/entities/errors/invalid-name-error'
 import { UserData } from '@/entities/user-data'
 import { left } from '@/shared/either'
 
@@ -39,5 +40,22 @@ describe('Register user on mailing list use case', () => {
 
     expect(user).toBeNull()
     expect(response).toEqual(left(new InvalidEmailError()))
+  })
+
+  test('should not add user with invalid name to mailing list', async () => {
+    const users: UserData[] = []
+
+    const repo: UserRepository = new InMemoryUserRepository(users)
+    const useCase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo)
+
+    const invalidName = ''
+    const email = 'john@doe.com'
+
+    const response = await useCase.registerUserOnMailingList({ name: invalidName, email })
+
+    const user = await repo.findUserByEmail(email)
+
+    expect(user).toBeNull()
+    expect(response).toEqual(left(new InvalidNameError()))
   })
 })
