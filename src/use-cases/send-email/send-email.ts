ce218f42add111e9d256aca825082f8c1f41ecf2
Lucas Eduardo <contato@lucaseduardo.io>
2022-08-11 10:08:01 -0300

    feat: handle invalid user data

--- src/use-cases/send-email/send-email.ts
+++ src/use-cases/send-email/send-email.ts
@@ -1,5 +1,6 @@
-import { UserData } from '@/entities'
-import { Either } from '@/shared'
+import { User, UserData } from '@/entities'
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
+import { Either, left } from '@/shared'
 import { MailServiceError } from '@/use-cases/errors'
 import { UseCase } from '@/use-cases/ports'
 import { EmailOptions, EmailService } from '@/use-cases/send-email/ports'
@@ -13,12 +14,20 @@ export class SendEmail implements UseCase {
     this.emailService = emailService
   }
 
-  perform (userData: UserData): Promise<Either<MailServiceError, EmailOptions>> {
-    const greetings = `E aí <b>${userData.name}</b>, beleza?`
+  async perform (userData: UserData): Promise<Either<InvalidNameError | InvalidEmailError | MailServiceError, EmailOptions>> {
+    const userOrError = User.create(userData)
+
+    if (userOrError.isLeft()) {
+      return left(userOrError.value)
+    }
+
+    const user = userOrError.value
+
+    const greetings = `E aí <b>${user.name}</b>, beleza?`
 
     const emailInfo: EmailOptions = {
       ...this.emailOptions,
-      to: `${userData.name} <${userData.email}>`,
+      to: `${user.name} <${user.email}>`,
       html: `${greetings}<br><br>${this.emailOptions.html}`
     }
 
