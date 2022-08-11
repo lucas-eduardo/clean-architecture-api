165136647c145ba1c1bf8a2f51ba1412abe028fa
Lucas Eduardo <contato@lucaseduardo.io>
2022-08-10 22:32:34 -0300

    feat: mongodb helper module

---
+++ src/external/repositories/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,21 @@
+import { MongoClient, Collection } from 'mongodb'
+
+export const MongoHelper = {
+  client: null as MongoClient,
+
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(uri)
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+  },
+
+  async getCollection (name: string): Promise<Collection> {
+    return await this.client.db().collection(name)
+  },
+
+  async clearCollection (name: string): Promise<void> {
+    await this.client.db().collection(name).deleteMany({})
+  }
+}
