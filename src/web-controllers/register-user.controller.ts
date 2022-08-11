faa6466677449779fa144b8388dcab34b0d6c4fa
Lucas Eduardo <contato@lucaseduardo.io>
2022-08-11 09:14:34 -0300

    refactor: remove unused conditional

--- src/web-controllers/register-user.controller.ts
+++ src/web-controllers/register-user.controller.ts
@@ -29,9 +29,7 @@ export class RegisterUserController {
         return badRequest(response.value)
       }
 
-      if (response.isRight()) {
-        return created(response.value)
-      }
+      return created(response.value)
     } catch (error) {
       return serverError(error)
     }
